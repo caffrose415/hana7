1b9428ae8ed4401bfc646e64bffde4c28ed652b2
caffrose415 <super_vet@naver.com>
2025-04-09 15:48:18 +0900

    modify example

--- js/ch9_example.js
+++ js/ch9_example.js
@@ -1,29 +1,29 @@
 function makeArray(n) {
-  if (n === 1) {
-    return [1];
-  }
-  return [...makeArray(n - 1), n];
+    if (n === 1) {
+        return [1];
+    }
+    return [...makeArray(n - 1), n];
 }
 
 function makeReverseArray(n) {
-  if (n === 1) {
-    return [1];
-  }
-  return [n, ...makeReverseArray(n - 1)];
+    if (n === 1) {
+        return [1];
+    }
+    return [n, ...makeReverseArray(n - 1)];
 }
 
 function makeArrayTCO(n, acc = []) {
-  if (n === 0) {
-    return acc;
-  }
-  return makeArrayTCO(n - 1, [n, ...acc]);
+    if (n === 0) {
+        return acc;
+    }
+    return makeArrayTCO(n - 1, [n, ...acc]);
 }
 
 function makeReverseArrayTCO(n, acc = []) {
-  if (n === 0) {
-    return acc;
-  }
-  return makeReverseArrayTCO(n - 1, [...acc, n]);
+    if (n === 0) {
+        return acc;
+    }
+    return makeReverseArrayTCO(n - 1, [...acc, n]);
 }
 
 console.log(makeArray(10));
