cda5c42f7bfb196fdf2ea0c4deebd2bf9d06fc93
unknown <super_vet@naver.com>
2025-07-15 09:01:10 +0900

    [25-07-14] Java

---
+++ java/basic/src/oopex3/Main.java
@@ -0,0 +1,9 @@
+package oopex3;
+
+import lombok.Getter;
+
+@Getter
+public class Main {
+	private String name;
+	private int age;
+}
---
+++ java/basic/src/oopex3/StreamEx.java
@@ -0,0 +1,31 @@
+package oopex3;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class StreamEx {
+	public static void main(String[] args) {
+		List<Integer> list = List.of(10, 6, 3, 3, 5, 4, 2, 7, 7, 9, 8, 10);
+
+		System.out.print("짝수의 개수 : ");
+		System.out.println(list.stream().filter(value -> value % 2 == 0).count());
+		System.out.print("각 숫자를 제곱 : ");
+		System.out.println(list.stream().map(value -> value * value).toList());
+		System.out.print("중복 제거 : ");
+		System.out.println(list.stream().distinct().toList());
+		System.out.print("기본 정렬 : ");
+		System.out.println(list.stream().sorted().toList());
+		System.out.print("역순(내림차순) 정렬 : ");
+		System.out.println(list.stream().sorted((o1, o2) -> o2 - o1).toList());
+		System.out.print("처음 5개만 출력 : ");
+		System.out.println(list.stream().limit(5).toList());
+		System.out.print("처음 5개 건너뛰고 출력 : ");
+		System.out.println(list.stream().skip(5).toList());
+		System.out.print("값이 5보다 큰 것만 출력 : ");
+		System.out.println(list.stream().filter(value -> value > 5).toList());
+		System.out.print("1 ~ 10의 합계 : ");
+		System.out.println(IntStream.range(1, 11).reduce(0, Integer::sum));
+		System.out.print("1 ~ 10의 평균 : ");
+		System.out.println(IntStream.range(1, 11).average().orElse(1.0));
+	}
+}
