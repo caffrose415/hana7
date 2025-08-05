a67c7b6251275838f23fea506e16b79a882c271e
unknown <super_vet@naver.com>
2025-08-05 11:15:00 +0900

    [25-08-05]

--- spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/controller/MemberController.java
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/controller/MemberController.java
@@ -1,31 +1,125 @@
 package com.hana7.springdemo.jpa.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.coobird.thumbnailator.Thumbnailator;
+
 import com.hana7.springdemo.board.dto.SearchCond;
 import com.hana7.springdemo.jpa.dto.MemberDTO;
+import com.hana7.springdemo.jpa.dto.UploadRequestDTO;
+import com.hana7.springdemo.jpa.dto.UploadResponseDTO;
 import com.hana7.springdemo.jpa.service.MemberService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/members")
+@RequiredArgsConstructor
+@Log4j2
 public class MemberController {
 	private final MemberService service;
 
-	public MemberController(MemberService service) {
-		this.service = service;
+	@Value("${upload.path}")
+	private String uploadPath = "src/main/resources";
+
+	@Tag(name = "file upload")
+	@Operation(summary = "Upload POST Member")
+	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public List<UploadResponseDTO> upload(UploadRequestDTO dto) {
+		log.info("upfileDto={}", dto);
+		log.info("uploadPath={}", uploadPath);
+
+		List<UploadResponseDTO> upfiles = new ArrayList<>();
+		if (dto.getFiles() != null) {
+			dto.getFiles().forEach(file -> {
+				String orgFname = file.getOriginalFilename();
+				log.info("orgName={}", orgFname + "::" + file.getSize() / 1024);
+				String uuid = UUID.randomUUID().toString();
+				String savedFname = uuid + "_" + orgFname;
+				Path upfilePath = Paths.get(uploadPath, savedFname);
+				try {
+					Path uploadDir = Paths.get(uploadPath);
+					if (!Files.exists(uploadDir)) {
+						Files.createDirectories(uploadDir);
+					}
+					file.transferTo(upfilePath);
+
+					boolean isImage = Files.probeContentType(upfilePath).startsWith("image");
+					if (isImage) {
+						File thumbnail = new File(uploadPath, "thumb_" + uuid + "_" + orgFname);
+						Thumbnailator.createThumbnail(upfilePath.toFile(), thumbnail, 200, 200);
+
+					}
+
+					upfiles.add(UploadResponseDTO.builder()
+						.orgFname(orgFname)
+						.fname(savedFname)
+						.isImage(isImage)
+						.build()
+					);
+
+				} catch (IOException e) {
+					log.error(e.getMessage());
+					throw new RuntimeException(e);
+				}
+			});
+		}
+
+		return upfiles;
+	}
+
+	@Tag(name = "Download File")
+	@Operation(summary = "Download Image")
+	@GetMapping("/view/{fileName}")
+	public ResponseEntity<Resource> viewFile(@PathVariable String fileName) throws IOException {
+		Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_TYPE, Files.probeContentType(resource.getFile().toPath()));
+		return ResponseEntity.ok().headers(headers).body(resource);
+	}
+
+	@Tag(name = "Delete File")
+	@Operation(summary = "Delete Image")
+	@GetMapping("/delete/{fileName}")
+	public Map<String, Boolean> deleteFile(@PathVariable String fileName) throws IOException {
+		Map<String, Boolean> resMap = new HashMap<>();
+		Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+		resMap.put(resource.getFile().getName(), resource.getFile().delete());
+
+		if (Files.probeContentType(resource.getFile().toPath()).startsWith("image")) {
+			File thumbnail = new File(uploadPath, "thumb_" + fileName);
+
+			resMap.put(thumbnail.getName(), thumbnail.delete());
+		}
+		return resMap;
 	}
 
 	@GetMapping()
